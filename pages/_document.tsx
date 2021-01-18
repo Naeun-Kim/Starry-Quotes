0160064b59e975af8041e2ed2524c2b5185f0b2b
naeun kim <naeunkim@naeunui-MacBookAir.local>
2021-01-18 16:04:05 +0900

    add meta and og tag to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,31 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="title" content="Starry Quotes" />
+          <meta
+            name="keywords"
+            content="quotes, inspiration, inspirational quote"
+          />
+          <meta
+            name="description"
+            content="Starry Quotes - Quotes in the sky"
+          />
+          <meta name="author" content="Naeun Kim | naeun.off@gmail.com" />
+          <meta name="copyright" content="Naeun Kim" />
+          <meta name="robots" content="ALL" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Starry Quotes" />
+          <meta
+            property="og:url"
+            content="https://starry-quotes.netlify.app/"
+          />
+          <meta property="og:site_name" content="Starry Quotes" />
+          <meta property="og:description" content="Quotes in the sky" />
+          <meta
+            property="og:image"
+            content="https://2019naeun.s3.ap-northeast-2.amazonaws.com/assets/images/starry-og.png"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Codystar&family=Roboto:ital,wght@1,100&display=swap"
